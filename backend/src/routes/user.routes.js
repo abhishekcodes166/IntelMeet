5b9d90f4f6bd9aa7ef454fa0afe037ab59fbaafd
Abhishek Jha <abhishekjhaa340@gmail.com>
2026-05-26 22:41:02 +0545

    deployed on render, 7

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { loginUser, registerUser, getCurrentUser } from "../controllers/user.controller.js";
+
+import {
+  loginUser,
+  registerUser,
+  getCurrentUser,
+} from "../controllers/user.controller.js";
+
 import verifyjwt from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
 router.get("/current-user", verifyjwt, getCurrentUser);
-router.post("/login",loginUser)
+
 export default router;
